570410b0622593ff27bc2d2582aef4ac0f303b9c
twmrsmrsm <twmrsmrsm.work@yandex.ru>
2018-10-05 15:36:50 +0300

    11test05102018

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main() {
     int a = 2;
-    int b = 3;
-    std::cout << b * a << '\n';
-    std::cout << a - b << '\n';
+    std::cout << a * a + a << '\n';
+    std::cout << a - a - a/a << '\n';
+
     return 0;
 }
